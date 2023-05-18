953072d3e44d7f7dab36568f7ea9bc2d643ccb76
Y0shicon <118278114+Y0shicon@users.noreply.github.com>
2023-05-18 21:28:05 +0530

    Delete Tests directory

--- Tests/Test 3/app.js
+++
@@ -1,9 +0,0 @@
-scrollBox = document.querySelector("#scrollBox")
-button = document.querySelector("input")
-
-button.addEventListener("click", scroll)
-
-function scroll () {
-    scrollBox.style.transform = "translateX(-50%)"
-    console.log("Scrolled")
-}
